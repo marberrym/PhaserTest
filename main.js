cb4c6aa8372da0647ebdd88def74b407ba26812c
Matthew <marberrym@gmail.com>
2019-01-05 18:28:09 -0500

    Getting closer to adding buttons :D

--- main.js
+++ main.js
@@ -18,14 +18,14 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-   
-    
     this.load.image('logo', 'assets/rnmlogo.png');
     this.load.image('sky', 'assets/nebula.jpg');
     this.load.image('rick', 'assets/rick.png');
     this.load.image('blue', 'assets/blue-flare.png');
     this.load.image('morty', 'assets/morty.png');
     this.load.image('greenorb', 'assets/greenorb.png');
+    this.load.image('button', 'assets/button.png');
+    this.load.image('rickraygun', 'assets/rickraygun.png');
 }
 
 function create ()
@@ -55,39 +55,41 @@ function create ()
         boundsAlignH: "center", // bounds center align horizontally
         boundsAlignV: "middle" // bounds center align vertically
     }
-
-
-    this.playButtonBox = new Phaser.Geom.Rectangle(250, 200, 120, 60);
-    var boxFill = this.add.graphics({ fillStyle: { color: 0x0000ff } });
-    boxFill.fillRectShape(this.playButtonBox);
-
     
-    this.playButton = this.add.text(275, 215, 'PLAY!', textStyle);
+    
     this.devButton = this.add.text(600, 200, 'About the Developer!');
 
-    this.devButton.setInteractive();
-    this.playButtonBox.setInteractive();
+    this.playBtn = this.add.sprite(300, 200, 'button').setScale(.25, .25);
+    this.playButton = this.add.text(275, 175, 'PLAY!', textStyle);
 
-    this.playButtonBox.on('pointerup', () => console.log("Start Game!"))
-    this.playButtonBox.on('pointerover', () => {
-        console.log("hover");
-        this.hoverState(playButtonBox);
-    })
+    this.devButton.setInteractive();
+    this.playBtn.setInteractive();
+
+    // this.playButtonBox.on('pointerup', () => console.log("Start Game!"))
+    // this.playButtonBox.on('pointerover', () => {
+    //     console.log("hover");
+    //     this.hoverState(playButtonBox);
+    // })
 
     var Rick = this.physics.add.image(500, 100, 'rick');
-    var Morty = this.physics.add.image(100, 50, 'morty');
+    var Rick2 = this.physics.add.image(300, 100, 'rickraygun').setScale(.25, .25);
+    var Morty = this.physics.add.image(100, 50, 'morty').setScale(.5, .5);
 
     Morty.setBounce(1, .8);
+
     
     Morty.setCollideWorldBounds(true);
 
     Rick.setBounce(1, .5);
     Rick.setCollideWorldBounds(true);
+    Rick2.setBounce(1, .7);
+    Rick2.setCollideWorldBounds(true);
 
     emitter2.startFollow(Rick);
     emitter.startFollow(Morty);
 }
 
 function hoverState(button) {
-    this.button.setStyle({fill: '#ff0'})
+    var btn = button;
+    btn.fillStyle({})
 }
